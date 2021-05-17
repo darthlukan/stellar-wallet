752c45f3e54366c14696ebbe3f940036d29f3354
Brian Tomlinson <btomlins@redhat.com>
2021-05-17 11:07:52 -0700

    Wire up styles

---
+++ src/components/stellar-wallet/stellar-wallet.tsx
@@ -0,0 +1,19 @@
+import { Component, Host, h } from '@stencil/core';
+import * as StellarSdk from "stellar-sdk";
+
+@Component({
+  tag: 'stellar-wallet',
+  styleUrl: 'stellar-wallet.scss',
+  shadow: true,
+})
+export class StellarWallet {
+  render() {
+    return [
+      <h1>
+        {!!StellarSdk
+          ? "The StellarSdk is ready to rock 🤘"
+          : "Uh oh, the StellarSdk is missing 😱"}
+      </h1>,
+    ];
+  }
+}
